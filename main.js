4e63381a5594eb54e6ad24dd9cd7cb2dbc36eb12
Diogo Ferreira Oliveira <99719843+diogofoliveir@users.noreply.github.com>
2022-03-11 17:13:13 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//bouton qui change la couleur de mon background
+ 
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    }
+
+
+
+  //mon image animation 
+
+//     $(".hover").mouseleave(
+//     function () {
+//       $(this).removeClass("hover");
+//     }
+//   );
+  
+ 
